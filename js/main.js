a403a72fbf776a410e8af95f3733b045f0d97dd7
edcsu <38857594+edcsu@users.noreply.github.com>
2019-06-26 10:05:52 +0300

    fix: set aria-label of view details button

--- js/main.js
+++ js/main.js
@@ -181,7 +181,8 @@ createRestaurantHTML = (restaurant) => {
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  more.setAttribute( "aria-label", "View Details");
+  li.append(more);
 
   return li
 }
